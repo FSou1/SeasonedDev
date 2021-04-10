845dd021ee6cfb92a3463e1d75c6279cd510f6fa
Maxim Zhukov <maxim.zhukov.dev@gmail.com>
2021-04-10 17:09:12 -0500

    Clean up the cat

--- typescript/deno/introduction/examples/cat/index.ts
+++ typescript/deno/introduction/examples/cat/index.ts
@@ -10,21 +10,6 @@ for (const name of filenames) {
 }
 
 /**
- * Read a file with a standard Deno API and suppress the NotFound exception
- * @param  {string} filename
- * @returns Promise
- */
-async function readFile(filename: string): Promise<Deno.File | null> {
-  return Deno.open(filename, { read: true }).catch((err) => {
-    if (err instanceof Deno.errors.NotFound) {
-      return null;
-    }
-
-    throw err;
-  });
-}
-
-/**
  * Read a file with the std/fs module and suppress the NotFound exception
  * Note: The --unstable flag is required
  * @param  {string} filename
