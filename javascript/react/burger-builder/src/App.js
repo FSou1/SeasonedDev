0107eb5cfba16870d448ee0f51b742bfc9fa7cde
Maxim Zhukov <maxim.zhukov.dev@gmail.com>
2021-07-11 15:05:20 -0500

    Fix minor issues

--- javascript/react/burger-builder/src/App.js
+++ javascript/react/burger-builder/src/App.js
@@ -1,4 +1,4 @@
-import Layout from './components/Layout/Layout';
+import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
