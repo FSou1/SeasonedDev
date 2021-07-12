3eceb63108534e2dc117db1dd273bcc0b7359cad
Maxim Zhukov <maxim.zhukov.dev@gmail.com>
2021-07-12 13:11:37 -0500

    Upload the 'import css classes' project

---
+++ typescript/react/import-css-classes/src/App.css.d.ts
@@ -0,0 +1,5 @@
+export const app: string;
+export const appLogo: string;
+export const appLogoSpin: string;
+export const appHeaderrFake: string;
+export const appLink: string;
--- typescript/react/import-css-classes/src/App.tsx
+++ typescript/react/import-css-classes/src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={classes.app}>
+      <header className={classes.appHeader}>
+        <img src={logo} className={classes.appLogo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={classes.appLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
