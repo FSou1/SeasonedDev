c118c200a94b05d048dc7bfd02fb70230eec6480
Maxim Zhukov <maxim.zhukov.dev@gmail.com>
2021-07-20 22:51:39 -0500

    Remove the custom.js

--- typescript/react/portfolio/src/components/Nav/Nav.tsx
+++ typescript/react/portfolio/src/components/Nav/Nav.tsx
@@ -1,52 +1,73 @@
-const Nav = () => (
-    <nav className="navbar navbar-expand-sm navbar-light">
-        <div className="container">
-            <a className="navbar-brand" href="index.html">
-                <i className='uil uil-user'></i> Marvel
-            </a>
+import { useEffect } from 'react';
 
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                <span className="navbar-toggler-icon"></span>
-                <span className="navbar-toggler-icon"></span>
-            </button>
+const Nav = () => {
+    const initNavbar = () => {
+        const navbar = $(".navbar") as any;
+        navbar.headroom();
+    };
 
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav mx-auto">
-                    <li className="nav-item">
-                        <a href="#about" className="nav-link">
-                            <span data-hover="About">About</span>
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#project" className="nav-link">
-                            <span data-hover="Projects">Projects</span>
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#resume" className="nav-link">
-                            <span data-hover="Resume">Resume</span>
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#contact" className="nav-link">
-                            <span data-hover="Contact">Contact</span>
-                        </a>
-                    </li>
-                </ul>
+    const initColorMode = () => {
+        $('.color-mode').click(function(){
+            $('.color-mode-icon').toggleClass('active')
+            $('body').toggleClass('dark-mode')
+        });
+    };
 
-                <ul className="navbar-nav ml-lg-auto">
-                    <div className="ml-lg-4">
-                        <div className="color-mode d-lg-flex justify-content-center align-items-center">
-                            <i className="color-mode-icon"></i>
-                            Color mode
+    useEffect(() => {
+        initNavbar();
+        initColorMode();
+    });
+
+    return (
+        <nav className="navbar navbar-expand-sm navbar-light">
+            <div className="container">
+                <a className="navbar-brand" href="index.html">
+                    <i className='uil uil-user'></i> Marvel
+                </a>
+
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav mx-auto">
+                        <li className="nav-item">
+                            <a href="#about" className="nav-link">
+                                <span data-hover="About">About</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#project" className="nav-link">
+                                <span data-hover="Projects">Projects</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#resume" className="nav-link">
+                                <span data-hover="Resume">Resume</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#contact" className="nav-link">
+                                <span data-hover="Contact">Contact</span>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <ul className="navbar-nav ml-lg-auto">
+                        <div className="ml-lg-4">
+                            <div className="color-mode d-lg-flex justify-content-center align-items-center">
+                                <i className="color-mode-icon"></i>
+                                Color mode
+                            </div>
                         </div>
-                    </div>
-                </ul>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-);
+        </nav>
+    );
+}
 
 export default Nav;
