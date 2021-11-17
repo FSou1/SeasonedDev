47ab6ee166bfd6c7053481b03e753cfa3160c2b1
Maxim Zhukov <maxim.zhukov.dev@gmail.com>
2021-11-16 20:20:08 -0600

    feat(loan-calc): add the source code

---
+++ typescript/react/loan-calc/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import LoanCalculator from './LoanCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LoanCalculator />
+      </header>
+    </div>
+  );
+}
+
+export default App;
