f43ef3be181565f597dd50245d311be286a37748
Maxim Zhukov <maxim.zhukov.dev@gmail.com>
2022-01-01 14:02:07 -0600

    feat(deno): add graphql query/mutation examples

---
+++ typescript/deno/graphql/tutorial_a/02/main.ts
@@ -0,0 +1,21 @@
+import { Server } from 'https://deno.land/std@0.107.0/http/server.ts'
+import { GraphQLHTTP } from 'https://deno.land/x/gql@1.1.0/mod.ts'
+import { makeExecutableSchema } from 'https://deno.land/x/graphql_tools@0.0.2/mod.ts'
+import { resolvers } from './resolvers.ts';
+import { typeDefs } from './typedefs.ts';
+
+const s = new Server({
+  handler: async (req) => {
+    const { pathname } = new URL(req.url)
+
+    return pathname === '/graphql'
+      ? await GraphQLHTTP<Request>({
+        schema: makeExecutableSchema({ resolvers, typeDefs }),
+        graphiql: true
+      })(req)
+      : new Response('Not Found', { status: 404 })
+  },
+  addr: ':3000'
+})
+
+s.listenAndServe()
---
+++ typescript/deno/graphql/tutorial_a/02/typedefs.ts
@@ -0,0 +1,3 @@
+import { importQL } from "https://deno.land/x/importql/mod.ts";
+
+export const typeDefs = importQL('query.graphql');
